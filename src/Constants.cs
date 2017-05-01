45de76b4eec6e8b7dcc5141c7a3353b4371035b7
Casper Thygesen <ct@mediaworkers.dk>
2017-05-01 11:44:55 +0200

    fixed some bad paths

--- src/Constants.cs
+++ src/Constants.cs
@@ -8,7 +8,7 @@ namespace SiteImprove.EPiserver.Plugin
 {
     public class Constants
     {
-        public const string SiteImproveTokenUrl = "https://my2.siteimprove.com/auth/token";
+        public const string SiteImproveTokenUrl = "https://my2.siteimprove.com/auth/token"; //"https://overlay.siteimprove.com/auth/token"; //"https://my2.siteimprove.com/auth/token";
         public const string SiteImproveRecheckUrl = "https://api-gateway.siteimprove.com/cms-recheck";
     }
 }
