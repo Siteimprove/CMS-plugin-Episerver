60d9d3a6c945b57a8aca33ecfa1042034cc0b7ef
Unknown <ct@kruso.dk>
2018-02-26 16:08:39 +0100

    fixed namespaces and module.config

--- src/Episerver.Plugin/Properties/VersionInfo.cs
+++ src/Episerver.Plugin/Properties/VersionInfo.cs
@@ -13,8 +13,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.2.2")]
-[assembly: AssemblyFileVersion("2.2.0.42513")]
-[assembly: AssemblyInformationalVersion("2.2.2")]
+[assembly: AssemblyVersion("2.3.3")]
+[assembly: AssemblyFileVersion("2.3.0.43726")]
+[assembly: AssemblyInformationalVersion("2.3.3")]
 
 
