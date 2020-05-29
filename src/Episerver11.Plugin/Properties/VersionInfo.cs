87a8adf1a4f24e0b747aab36e7d0d9df20386cf5
kentak-74 <ken.takeura@valtech.com>
2020-05-29 11:42:12 +0200

    Updated nuget build version to 2.7.0

--- src/Episerver11.Plugin/Properties/VersionInfo.cs
+++ src/Episerver11.Plugin/Properties/VersionInfo.cs
@@ -13,8 +13,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.6.32")]
-[assembly: AssemblyFileVersion("2.6.0.25854")]
-[assembly: AssemblyInformationalVersion("2.6.32")]
+[assembly: AssemblyVersion("2.7.0")]
+[assembly: AssemblyFileVersion("2.7.0.30008")]
+[assembly: AssemblyInformationalVersion("2.7.0")]
 
 
