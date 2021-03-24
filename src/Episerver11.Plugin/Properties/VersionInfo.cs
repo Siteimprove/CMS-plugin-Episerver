d6df09b93912692bdf3b2f4861c5cdf1312fd106
Mads Storm Hansen <madsstorm@gmail.com>
2021-03-24 15:07:53 +0100

    Version update

--- src/Episerver11.Plugin/Properties/VersionInfo.cs
+++ src/Episerver11.Plugin/Properties/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.8.0")]
-[assembly: AssemblyFileVersion("2.8.0.31531")]
-[assembly: AssemblyInformationalVersion("2.8.0-beta902")]
+[assembly: AssemblyFileVersion("2.8.0.41100")]
+[assembly: AssemblyInformationalVersion("2.8.0-beta904")]
 
 
